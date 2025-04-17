2e567656b06eaba5e06b45c9461f5c920035b31f
evgeniy-kulikov <redstepway@gmail.com>
2025-04-17 17:48:19 +0300

    course-100707 v3

---
+++ andrey/link.py
@@ -0,0 +1,30 @@
+""""""
+
+"""
+"Поколение Python": курс для начинающих  (Тимур Гуев) 1
+https://stepik.org/course/58852/syllabus
+
+"Поколение Python": курс для продвинутых  (Тимур Гуев) 2
+https://stepik.org/course/68343/syllabus
+
+"Поколение Python": курс для профессионалов  (Тимур Гуев)  3 ($)
+https://stepik.org/course/82541/syllabus
+
+
+Добрый, добрый Python с Сергеем Балакиревым 1
+https://stepik.org/course/100707/syllabus
+
+
+Инди-курс программирования на Python  (Артем Егоров) 1
+https://stepik.org/course/63085/syllabus
+
+Функциональное программирование на Python  (Артем Егоров) - все про функции 2
+https://stepik.org/course/195619/syllabus
+
+
+Python от Python_Samurai: часть 1  (Никита Давыдов) 1
+https://stepik.org/course/186684/syllabus
+
+Python от Python_Samurai: часть 2  (Никита Давыдов) 2
+https://stepik.org/course/189107/promo
+"""
